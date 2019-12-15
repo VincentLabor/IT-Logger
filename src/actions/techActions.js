0088e6532b1786bdedde06b40b3db00c40b245a0
VincentLabor <vlabor95@gmail.com>
2019-12-14 19:19:40 -0800

    Users can now add technicians to the website

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -1,34 +1,59 @@
-import { 
-    GET_TECHS,
-    ADD_TECH,
-    DELETE_TECH,
-    TECHS_ERROR,
-    SET_LOADING
-} from './types';
+import {
+  GET_TECHS,
+  ADD_TECH,
+  DELETE_TECH,
+  TECHS_ERROR,
+  SET_LOADING
+} from "./types";
 
 export const getTechs = () => async dispatch => {
-    try {
-      setLoading();
-  
-      const res = await fetch("/techs");
-      const data = await res.json();
-  
-      dispatch({
-        type: GET_TECHS,
-        payload: data
-      });
-    } catch (error) {
-      dispatch({
-        type: TECHS_ERROR,
-        payload: error.response.statusText
-      });
-    }
-  };
+  try {
+    setLoading();
+
+    const res = await fetch("/techs");
+    const data = await res.json();
+
+    dispatch({
+      type: GET_TECHS,
+      payload: data
+    });
+  } catch (error) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: error.response.statusText
+    });
+  }
+};
+
+export const addTech = tech => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch("/techs", {
+      method: "POST",
+      body: JSON.stringify(tech),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_TECH,
+      payload: data
+    });
+  } catch (error) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: error.response.statusText
+    });
+  }
+};
 
 export const setLoading = () => {
-    //This will go to the reducer and set loading to true
-    return {
-      type: SET_LOADING
-    };
+  //This will go to the reducer and set loading to true
+  return {
+    type: SET_LOADING
   };
-  
+};
--- src/reducers/techReducer.js
+++ src/reducers/techReducer.js
@@ -16,10 +16,27 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case GET_TECHS:
       return {
-          ...state,
-          techs: action.payload,
-          loading: false
+        ...state,
+        techs: action.payload,
+        loading: false
       };
+    case ADD_TECH:
+      return {
+        ...state,
+        techs: [...state.techs, action.payload], //You want to remember to be specific here or else you will return the whole entire state.
+        loading: false
+      };
+    case TECHS_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false
+      };
+      case SET_LOADING:
+        return{
+          ...state,
+          loading: true
+        }
     default:
       return state;
   }
